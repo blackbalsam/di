a8876abbe5ad650a0a21bddeb02b30d8dd63cec2
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-08 19:05:46 +0300

    Dependency Injection using Spring Framework

--- src/main/java/gatis/springframework/di/controllers/ConstructorInjectedController.java
+++ src/main/java/gatis/springframework/di/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package gatis.springframework.di.controllers;
 
 import gatis.springframework.di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/gatis/springframework/di/services/impl/GreetingServiceImpl.java
+++ src/main/java/gatis/springframework/di/services/impl/GreetingServiceImpl.java
@@ -1,7 +1,9 @@
 package gatis.springframework.di.services.impl;
 
 import gatis.springframework.di.services.GreetingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GreetingServiceImpl implements GreetingService {
 
     @Override
