5608cb235fc65b8b992fae2a5a3d04f550a661db
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-08 19:55:41 +0300

    Spring Profiles

---
+++ src/main/java/gatis/springframework/di/services/impl/I18nEnglishGreetingService.java
@@ -0,0 +1,15 @@
+package gatis.springframework.di.services.impl;
+
+import gatis.springframework.di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18nService")
+public class I18nEnglishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/main/java/gatis/springframework/di/services/impl/I18nLatvianGreetingService.java
@@ -0,0 +1,15 @@
+package gatis.springframework.di.services.impl;
+
+import gatis.springframework.di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("LV")
+@Service("i18nService")
+public class I18nLatvianGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Sveika Pasaule - LV";
+    }
+}
