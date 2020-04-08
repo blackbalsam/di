25dbca7f8c08d3b931edf36c3e582ddde1bb5fa5
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-08 20:16:36 +0300

    Default Profile

--- src/main/java/gatis/springframework/di/services/impl/I18nLatvianGreetingService.java
+++ src/main/java/gatis/springframework/di/services/impl/I18nLatvianGreetingService.java
@@ -4,7 +4,7 @@ import gatis.springframework.di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("LV")
+@Profile({"LV", "default"})
 @Service("i18nService")
 public class I18nLatvianGreetingService implements GreetingService {
 
